53e59910b9e5b55787bd41c61cb403c9cd5495e4
Jon Preece <jonpreece@hotmail.co.uk>
2018-01-20 11:52:13 +0000

    Added chota

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-import')(),
     require('postcss-cssnext')(),
     require('postcss-modules')({
       generateScopedName: '[name]__[local]___[hash:base64:5]'
